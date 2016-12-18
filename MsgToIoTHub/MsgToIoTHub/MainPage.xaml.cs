0feb5c9edbd1f696a9754c0a042d96ef4d1f6575
Vedo <vedad_fejzagic@yahoo.com>
2016-12-18 02:25:35 +0200

    Dodano nesto lijepo opet

---
+++ MsgToIoTHub/MsgToIoTHub/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MsgToIoTHub
+{
+    public sealed partial class MainPage : Page
+    {
+
+        static string connectionString = "HostName=SecuritySysteHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=8xNbj+v0tzkzwLMlvLJiucEsbuy1pPUDdfMYtGzpRk0=";
+    
+        static ServiceClient serviceClient;
+
+        private static int i = 2;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (i % 2 == 0)
+                await SendCloudToDeviceMessageAsync("1");
+            else
+                await SendCloudToDeviceMessageAsync("0");
+            i++;
+
+        }
+        private async static Task SendCloudToDeviceMessageAsync(string podaci)
+        {
+            var commandMessage = new Microsoft.Azure.Devices.Message(Encoding.ASCII.GetBytes(podaci));
+            //promjenit deviceid
+            await serviceClient.SendAsync("Vedo", commandMessage);
+        }
+    }
+}
